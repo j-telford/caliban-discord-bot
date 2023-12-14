4bd58688f487c4a75c3eff9cf651674176574108
Jay Telford <jay.tes@telford.vip>
2023-12-14 13:40:09 +0000

    updated bot script so bot runs

--- bot.js
+++ bot.js
@@ -1,8 +1,17 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    // Add other intents as needed
+  ],
+});
 
 client.once("ready", () => {
   console.log("Bot is ready!");
 });
 
-client.login("YOUR_BOT_TOKEN");
+client.login(
+  "MTE4NDg0MzI1NjI2MjA1MzkyOA.Gl7b_p.vVzWq49oc_khIfE9i2unWq4ftzXh2OV9seuMEw"
+);
