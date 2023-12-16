7c277f932442435e04e8b0a1469b3939a7dfa40c
Jay Telford <jay.tes@telford.vip>
2023-12-16 14:46:25 +0000

    updated with a secret config file

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,42 @@ const gulp = require("gulp");
 const zip = require("gulp-zip");
 const del = require("del");
 const sftp = require("ssh2-sftp-client");
-const packageJson = require("./package.json");
+const config = require("config");
+
+// Define the task to SCP the zip archive to the server
+gulp.task("scp", async () => {
+  const client = new sftp();
+
+  try {
+    const connectionConfig = {
+      host: config.get("host"),
+      port: config.get("port"),
+      username: config.get("username"),
+    };
+
+    // Check if private key path is defined in config
+    if (config.has("privateKeyPath")) {
+      connectionConfig.privateKey = fs.readFileSync(
+        config.get("privateKeyPath")
+      );
+    }
+
+    // Check if password is defined in config
+    if (config.has("password")) {
+      connectionConfig.password = config.get("password");
+    }
+
+    await client.connect(connectionConfig);
+
+    // Update the remote path as needed
+    const remotePath = "~/discordbot/";
+
+    // Upload the zip file
+    await client.uploadDir("dist", remotePath);
+  } finally {
+    client.end();
+  }
+});
 
 // Define the task to create a zip archive
 gulp.task("zip", () => {
@@ -12,8 +47,8 @@ gulp.task("zip", () => {
     fs.mkdirSync("./dist");
   }
 
-  const packageName = packageJson.name;
-  const version = packageJson.version;
+  const packageName = config.get("name");
+  const version = config.get("version");
   const zipName = `${packageName}-${version}.zip`;
 
   return gulp
@@ -27,29 +62,6 @@ gulp.task("zip", () => {
     ])
     .pipe(zip(zipName))
     .pipe(gulp.dest("./dist"));
-});
-
-// Define the task to SCP the zip archive to the server
-gulp.task("scp", async () => {
-  const client = new sftp();
-
-  try {
-    await client.connect({
-      host: "mythbound.online",
-      port: 22,
-      username: "jay",
-      privateKey: fs.readFileSync("C:/Users/telfo/.ssh/id_rsa"),
-      passphrase: "superdooper1",
-    });
-
-    // Update the remote path as needed
-    const remotePath = "/home/jay/caliban/dist";
-
-    // Upload the zip file
-    await client.uploadDir("dist", remotePath);
-  } finally {
-    client.end();
-  }
 });
 
 // Define the task to clean the /dist directory
