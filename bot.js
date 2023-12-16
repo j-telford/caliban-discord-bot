f60a9a539f887b575ee3264e652b6efb20f14785
Jay Telford <jay.tes@telford.vip>
2023-12-16 11:48:18 +0000

    updated bot script for rss scraping

--- bot.js
+++ bot.js
@@ -1,16 +1,49 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const Parser = require("rss-parser");
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    // Add other intents as needed
-  ],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
+
+const parser = new Parser();
+const articleCache = new Set();
 
 client.once("ready", () => {
   console.log("Bot is ready!");
+
+  // Initial fetch and post
+  fetchAndPostArticles();
+
+  // Schedule fetching and posting every 4 hours
+  setInterval(fetchAndPostArticles, 4 * 60 * 60 * 1000);
 });
+
+// Function to fetch and post new articles
+async function fetchAndPostArticles() {
+  try {
+    // Parse the RSS feed
+    const feed = await parser.parseURL("https://mythbound.online/rss");
+
+    // Find the "published-articles" channel
+    const channel = client.channels.cache.find(
+      (ch) => ch.name === "published-articles" && ch.type === "GUILD_TEXT"
+    );
+
+    if (channel) {
+      // Post links to new articles
+      feed.items.forEach((item) => {
+        if (!articleCache.has(item.link)) {
+          channel.send(item.link);
+          articleCache.add(item.link);
+        }
+      });
+    } else {
+      console.error('Channel "published-articles" not found!');
+    }
+  } catch (error) {
+    console.error("Error fetching or posting articles:", error.message);
+  }
+}
 
 client.login(
   "MTE4NDg0MzI1NjI2MjA1MzkyOA.Gl7b_p.vVzWq49oc_khIfE9i2unWq4ftzXh2OV9seuMEw"
