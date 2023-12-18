59610458b87f374e199243375fbaaf534c80cba4
Jay Telford <hello@jaytelford.me>
2023-12-18 13:41:35 +0000

    updated to guildID and channelID

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 const Parser = require("rss-parser");
+
 if (process.env.NODE_ENV === "development") {
   const dotenv = require("dotenv");
   dotenv.config();
@@ -12,11 +13,11 @@ const client = new Client({
 const parser = new Parser();
 const articleCache = new Set();
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("Bot is ready!");
 
   // Initial fetch and post
-  fetchAndPostArticles();
+  await fetchAndPostArticles();
 
   // Schedule fetching and posting every 4 hours
   setInterval(fetchAndPostArticles, 4 * 60 * 60 * 1000);
@@ -28,22 +29,20 @@ async function fetchAndPostArticles() {
     // Parse the RSS feed using the environment variable directly
     const feed = await parser.parseURL(process.env.RSS_FEED_URL);
 
-    // Find the "published-articles" channel
-    const channel = client.channels.cache.find(
-      (ch) => ch.name === "published-articles" && ch.type === "GUILD_TEXT"
-    );
-
-    if (channel) {
-      // Post links to new articles
-      feed.items.forEach((item) => {
-        if (!articleCache.has(item.link)) {
-          channel.send(item.link);
-          articleCache.add(item.link);
-        }
-      });
-    } else {
+    // Find the "published-articles" channel by ID from the environment variable
+    const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+    if (!channel) {
       console.error('Channel "published-articles" not found!');
+      return;
     }
+
+    // Post links to new articles
+    feed.items.forEach((item) => {
+      if (!articleCache.has(item.link)) {
+        channel.send(item.link);
+        articleCache.add(item.link);
+      }
+    });
   } catch (error) {
     console.error("Error fetching or posting articles:", error.message);
   }
